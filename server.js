2b992db9cb617c07fb8898210c66c415608caab1
Jonathan Alexander <me@jmalexan.com>
2018-12-22 23:38:32 -0800

    add xur endpoint, remove unnecessary endpoints, add node_env to dockerfile

--- server.js
+++ server.js
@@ -1,52 +1,18 @@
 const express = require('express');
-const request = require('request-promise-native');
 const fs = require('fs');
 
 var app = express();
 
-let data;
-
 app.get('/data', (req, res) => {
+    let data = JSON.parse(fs.readFileSync('storage/data.json'));
     res.send(data);
 })
 
-refreshdata().then(() => {
+app.get('/xur', (req, res) => {
+    let data = JSON.parse(fs.readFileSync('storage/xur.json'));
+    res.send(data);
+})
 
-    runRefresh();
-
-    app.listen(80, () => {
-        console.log(`Server listening on port 80...`);
-    });
+app.listen(80, () => {
+    console.log(`Server listening on port 80...`);
 });
-
-function runRefresh() {
-    refreshdata().then(() => {
-        let currentMs = (new Date()).getTime();
-        let msThroughDay = currentMs % 86400000;
-        let nextRun;
-        if (msThroughDay > 61200000) {
-            nextRun = 86400000 - msThroughDay;
-        } else {
-            nextRun = 61200000 - msThroughDay;
-        }        
-        queueNext(nextRun);
-    }, () => {
-        queueNext(10000);
-    })    
-
-}
-
-async function refreshdata() {
-    let newdata = JSON.parse(fs.readFileSync('storage/data.json'));
-    if (newdata == data) {
-        throw 'DupeData';
-    } else {
-        data = newdata;
-    }
-}
-
-function queueNext(nextRun) {
-    setTimeout(() => {
-        runRefresh();
-    }, nextRun)
-}
