9c57925ada4f0990a0e0342e249404f22f4db877
Jonathan Alexander <me@jmalexan.com>
2019-05-02 21:08:03 -0700

    Revert "Merge branch 'master' of https://gitlab.com/KingsOfXur/wherethefuckisxur-server"
    This reverts commit 7f746094142cd74b7f93c90b9465c5c0108dedc2, reversing
    changes made to 5755d17d317d182ceec9b5782b187d4976cc081d.

--- server.js
+++ server.js
@@ -216,19 +216,9 @@ function updateCycleData() {
     let cyclesFile = fs.readFileSync(dataDir + '/cycles.json', 'utf8');
     cyclesAPI = JSON.parse(cyclesFile);
 
-    let cityStatus;
-    if (cyclesAPI.citystatus.id == 0) {
-        cityStatus = "Shit ain't fucked, yo";
-    } else if (cyclesAPI.citystatus.id == 1) {
-        cityStatus = "Shit is moderately fucked, yo";
-    } else {
-        cityStatus = "Shit is completely fucked, yo";
-    }
-
     data.cycles = {
         activenightfalls: cyclesAPI.activenightfalls,
-        dailies: cyclesAPI.dailies,
-        city: cityStatus
+        dailies: cyclesAPI.dailies
     }
 }
 
